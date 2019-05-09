fd91d7d4502bf9b703475f7ae11db2138421570d
Xurxe <xurxe.garcia@integrify.io>
2019-05-09 16:38:56 +0300

    Got hamburger menu toggling

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,109 +1,140 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 
 import './Nav.css';
 
 import NavA from './NavA';
 
-const handleClick = (e) => {
-    const node = ReactDOM.findDOMNode(e.target);
-    console.log(node)
+class Nav extends Component {
+
+    state = {
+        hidden: true,
+    }
+
+    handleClick = () => {
+        this.setState((prevState => ({hidden: !prevState.hidden})));
+        console.log(this.state.hidden)
+    };
+
+    render() {
+
+        if (this.state.hidden === true) {
+            return (
+                <nav className='Nav'>
+    
+                    <p className='Nav_p'>
+                        Ajak Majok
+                    </p>
+    
+                    <button 
+                    className='Nav_button'
+                    onClick={this.handleClick}
+                    >
+                        <i className="fas fa-bars"></i>
+    
+                    </button>
+    
+                </nav>
+            )
+        }
+
+        else {
+            return (
+                <StaticQuery 
+                query={
+                    graphql`
+                    query {
+                        contentfulNavigationBar{
+                            id
+                            internalLinks {
+                                ... on ContentfulIndex {
+                                    id
+                                    longTitle
+                                    iconFontAwesome
+                                }
+                                ... on ContentfulPage {
+                                    id
+                                    shortTitle
+                                    slug
+                                }
+                            }
+                            externalLinks {
+                                ... on ContentfulElectionLink {
+                                    id
+                                    name
+                                    url
+                                }
+                                ... on ContentfulSocialMediaLink {
+                                    id
+                                    name
+                                    url
+                                    iconFontAwesome
+                                }
+                            }
+                        }
+                    }    
+                `}
+                render={data => {
+                    const {contentfulNavigationBar} = data;
+                    const {internalLinks, externalLinks} = contentfulNavigationBar;
+                    const jsx = (
+                        <nav className='Nav'>
+            
+                            <p className='Nav_p'>
+                                Ajak Majok
+                            </p>
+            
+                            <button 
+                            className='Nav_button'
+                            onClick={this.handleClick}
+                            >
+                                <i className="fas fa-bars"></i>
+            
+                            </button>
+            
+                            <div 
+                            className={`Nav_div Nav_div___internalLinks`}>
+            
+                                {internalLinks.map(
+                                    (link) => (
+            
+                                        <NavA 
+                                        key={link.id}
+                                        link={link}
+                                        ></NavA>
+            
+                                    )
+                                )}
+            
+                            </div>
+            
+                            <div 
+                            className={`Nav_div Nav_div___externalLinks`}
+                            >
+            
+                                {externalLinks.map(
+                                    (link) => (
+            
+                                        <NavA 
+                                        key={link.id}
+                                        link={link}
+                                        ></NavA>
+            
+                                    )
+                                )}
+            
+                            </div>
+            
+                        </nav>
+                    );
+            
+                    return jsx;
+                }}
+                />
+            )
+        }
+
+    }
 }
-
-
-const Nav = () => (
-    <StaticQuery 
-    query={
-        graphql`
-        query {
-            contentfulNavigationBar{
-                id
-                internalLinks {
-                    ... on ContentfulIndex {
-                        id
-                        longTitle
-                        iconFontAwesome
-                    }
-                    ... on ContentfulPage {
-                        id
-                        shortTitle
-                        slug
-                    }
-                }
-                externalLinks {
-                    ... on ContentfulElectionLink {
-                        id
-                        name
-                        url
-                    }
-                    ... on ContentfulSocialMediaLink {
-                        id
-                        name
-                        url
-                        iconFontAwesome
-                    }
-                }
-            }
-        }    
-    `}
-    render={data => {
-        const {contentfulNavigationBar} = data;
-        const {internalLinks, externalLinks} = contentfulNavigationBar;
-        const jsx = (
-            <nav className='Nav'>
-
-                <p className='Nav_p'>
-                    Ajak Majok
-                </p>
-
-                <button 
-                className='Nav_button'
-                onClick={handleClick}
-                >
-                    <i className="fas fa-bars"></i>
-
-                </button>
-
-                <div 
-                className={`Nav_div Nav_div___internalLinks`}>
-
-                    {internalLinks.map(
-                        (link) => (
-
-                            <NavA 
-                            key={link.id}
-                            link={link}
-                            ></NavA>
-
-                        )
-                    )}
-
-                </div>
-
-                <div 
-                className={`Nav_div Nav_div___externalLinks`}
-                >
-
-                    {externalLinks.map(
-                        (link) => (
-
-                            <NavA 
-                            key={link.id}
-                            link={link}
-                            ></NavA>
-
-                        )
-                    )}
-
-                </div>
-
-            </nav>
-        );
-
-        return jsx;
-    }}
-    />
-);
 
 export default Nav;
