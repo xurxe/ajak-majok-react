bfb758ab02fe6ba30f4627ded4d0d186311e827b
Xurxe <xurxe.garcia@integrify.io>
2019-05-10 13:43:44 +0300

    Tried different approach with Nav, still buggy

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import ReactResizeDetector from 'react-resize-detector';
 
-
 import './Nav.css';
 
 import NavA from './NavA';
@@ -12,93 +11,101 @@ class Nav extends Component {
     constructor() {
         super();
         this.state = {
-            windowWidth: window.innerWidth,
-            hidden: true,
-        }
-    }
-
+            small: false,
+            hidden: false,
+            hiddenClass: '',
+        };
+    };
 
     handleWindowResize = () => {
-        this.setState((() => ({windowWidth: window.innerWidth})));
-        console.log(this.state.windowWidth)
+        if (window.matchMedia('(max-width: 800px)').matches) {
+            this.setState(() => ({
+                small: true,
+                hidden: true,
+                hiddenClass: 'Nav_div___hidden',
+            }))
+        }
+
+        else {
+            this.setState(() => ({
+                small: false, 
+                hidden: false,
+                hiddenClass: ''
+            }))
+        };
     }
 
     handleClick = () => {
-        this.setState(((prevState) => ({hidden: !prevState.hidden})));
+        this.setState((prevState) => (
+            {hidden: !prevState.hidden}
+        ));
+
+        if (this.setState.hidden) {
+            this.setState(() => (
+                {hiddenClass: 'Nav_div___hidden'}
+            ));
+        }
+
+        else {
+            this.setState(() => (
+                {hiddenClass: ''}
+            ));
+        };
     };
 
     render() {
 
-        if (this.state.windowWidth < 800 && this.state.hidden) {
-            return (
-
-                <ReactResizeDetector 
-                handleWidth 
-                onResize={this.handleWindowResize}
-                refreshMode='throttle'
-                >
-                
-                    <nav className='Nav'>
-                        
-                        <p className='Nav_p'>
-                            Ajak Majok
-                        </p>
-
-                        <button 
-                        className='Nav_button'
-                        onClick={this.handleClick}
-                        >
-                            <i className="fas fa-bars"></i>
-
-                        </button>
-
-                    </nav>
-
-                </ReactResizeDetector>
-
-            )
-        }
-
-        else {
-            return (
-                <StaticQuery 
-                query={
-                    graphql`
-                    query {
-                        contentfulNavigationBar{
-                            id
-                            internalLinks {
-                                ... on ContentfulIndex {
-                                    id
-                                    longTitle
-                                    iconFontAwesome
-                                }
-                                ... on ContentfulPage {
-                                    id
-                                    shortTitle
-                                    slug
-                                }
+        return (
+            <StaticQuery 
+            query={
+                graphql`
+                query {
+                    contentfulNavigationBar{
+                        id
+                        internalLinks {
+                            ... on ContentfulIndex {
+                                id
+                                longTitle
+                                iconFontAwesome
                             }
-                            externalLinks {
-                                ... on ContentfulElectionLink {
-                                    id
-                                    name
-                                    url
-                                }
-                                ... on ContentfulSocialMediaLink {
-                                    id
-                                    name
-                                    url
-                                    iconFontAwesome
-                                }
+                            ... on ContentfulPage {
+                                id
+                                shortTitle
+                                slug
                             }
                         }
-                    }    
-                `}
-                render={data => {
-                    const {contentfulNavigationBar} = data;
-                    const {internalLinks, externalLinks} = contentfulNavigationBar;
-                    const jsx = (
+                        externalLinks {
+                            ... on ContentfulElectionLink {
+                                id
+                                name
+                                url
+                            }
+                            ... on ContentfulSocialMediaLink {
+                                id
+                                name
+                                url
+                                iconFontAwesome
+                            }
+                        }
+                    }
+                }    
+            `}
+            render={data => {
+                const { contentfulNavigationBar } = data;
+                const { internalLinks, externalLinks } = contentfulNavigationBar;
+                const { hiddenClass } = this.state;
+
+                const jsx = (
+
+                    <ReactResizeDetector
+                    handleWidth
+                    onResize={this.handleWindowResize}
+                    refreshMode='throttle'
+                    refreshOptions={{
+                        leading: true, 
+                        trailing: false 
+                    }}
+                    >
                         <nav className='Nav'>
             
                             <p className='Nav_p'>
@@ -114,7 +121,7 @@ class Nav extends Component {
                             </button>
             
                             <div 
-                            className={`Nav_div Nav_div___internalLinks`}>
+                            className={`Nav_div Nav_div___internalLinks ${hiddenClass}`}>
             
                                 {internalLinks.map(
                                     (link) => (
@@ -130,7 +137,7 @@ class Nav extends Component {
                             </div>
             
                             <div 
-                            className={`Nav_div Nav_div___externalLinks`}
+                            className={`Nav_div Nav_div___externalLinks ${hiddenClass}`}
                             >
             
                                 {externalLinks.map(
@@ -147,15 +154,16 @@ class Nav extends Component {
                             </div>
             
                         </nav>
-                    );
-            
-                    return jsx;
-                }}
-                />
-            )
-        }
 
-    }
-}
+                    </ReactResizeDetector>
+
+                );
+        
+                return jsx;
+            }}
+            />
+        );
+    };
+};
 
 export default Nav;
